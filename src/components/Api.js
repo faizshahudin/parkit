6c2647f7f2f461e1d71685adae6590e0495a039f
amirabuhasan <amirabuhasan@gmail.com>
2018-04-24 19:06:13 +0800

    latest api method

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,18 +1,10 @@
 export const register = (data) =>
-    fetch(`http://127.0.0.1:8000/register/`, {
+    fetch(`https://cors-anywhere.herokuapp.com/http://127.0.0.1:8000/register/`, {
       method: 'POST',
       headers: {
         "Content-Type": "application/json",
-        "mode": 'no-cors',
       },
-      body: {
-        "username": "dsaaaa",
-        "first_name": "dsfs",
-        "last_name": "dsafdsa",
-        "email": "sndjfnsaa@gmail.com",
-        "email2": "sndjfnsaa@gmail.com",
-        "password": "test123"
-      }
+      body: JSON.stringify(data)
     })
     .then(res => res)
     .then(res => console.log(res))
