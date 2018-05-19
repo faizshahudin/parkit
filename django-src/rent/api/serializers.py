cdc5b918f018e1d965c49bce6d7436795ccf066a
Wong Jao Ern <jao.ern.wong@intel.com>
2018-05-19 19:29:50 +0800

    remove double fields
    Front-End will perform data checks on Rent Forms

--- django-src/rent/api/serializers.py
+++ django-src/rent/api/serializers.py
@@ -16,43 +16,18 @@ class ParkingForRentSerializer(ModelSerializer):
         fields = [
             'db_property',
             'db_area',
-            'sel_area',
             'db_type',
             'db_reserved',
             'db_period',
-            'sel_period',
             'db_price',
-            'sel_price',
             'db_status',
             'timestamp',
             'serial_no',
         ]
 
     def validate(self,data):
-        sel_area   = data.get('sel_area', None)
-        db_area    = data.get('db_area', None)
-        sel_period = data.get('sel_period', None)
-        db_period  = data.get('db_period', None)
-        sel_price  = data.get('sel_price', None)
-        db_price   = data.get('db_price', None)
         serial_no  = data.get('serial_no', None)
         unique_id  = get_random_string(length=10)
-
-        if sel_area is "Other" and db_area is "":
-            raise ValidationError("User Input Required")     
-        if sel_area is not "Other" and db_area is "":
-            data["db_area"] = data["sel_area"]
-
-        if sel_period is "Any" and db_period is "":
-            raise ValidationError("User Input Required")     
-        if sel_period is not "Any" and db_period is "":
-            data["db_period"] = data["sel_period"]
-
-        if sel_price is "Any" and db_price is "":
-            raise ValidationError("User Input Required")     
-        if sel_price is not "Any" and db_price is "":
-            data["db_price"] = data["sel_price"]
-        
         data["serial_no"] = serial_no + '#' + unique_id
         
         return data
--- django-src/rent/models.py
+++ django-src/rent/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+
 #from django.contrib.contenttypes.models import ContentType
 #from django.db.models.signals import pre_save
 
@@ -10,54 +11,22 @@ from django.utils import timezone
 
 # Create your models here.
 class ParkingForRent (models.Model):
-    user          = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    db_property   = models.CharField(max_length=100)
-    list_area = (
-        ('KL_Sentral' , 'KL Sentral'),
-        ('Damansara'  , 'Damansara'),
-        ('Other'      , 'Other'),
-    )    
-    sel_area = models.CharField(max_length=100, choices=list_area, default='Please Select Area')
-    db_area  = models.CharField(max_length=200,blank=True)
-    db_bay   = models.CharField(max_length=100,default='Insert Bay Info')
-    list_type = (
-        ('Condominium'   , 'Condominium'),
-        ('Commercial'    , 'Commercial'),
-        ('Landed'        , 'Landed'),
-    )
-   
-    db_type = models.CharField(max_length=100,choices=list_type, default='Condominium')
-  
-    list_option = (
-        ('yes', 'yes'),
-        ('no' , 'no'),
-    )
-    db_reserved = models.CharField(max_length=100,choices=list_option,default='yes')
-    list_period = (
-        ('12' , '12 Months'),
-        ('6'  , '6 Months'),
-        ('3'  , '4 Months'),
-        ('Any', 'Any'),
-    )
-    sel_period = models.CharField(max_length=100, choices=list_period, default=12)
-    db_period = models.CharField(max_length=100,blank=True)
-    list_price = (
-        ('300', 'RM300'),
-        ('250', 'RM250'),
-        ('150', 'RM150'),
-        ('Any', 'Any'),
-    )
-
-    sel_price = models.CharField(max_length=100, choices=list_price, default=300)
-    db_price = models.CharField(max_length=100, blank=True)
+    user        = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    db_property = models.CharField(max_length=100)
+    db_area     = models.CharField(max_length=200,blank=True)
+    db_bay      = models.CharField(max_length=100,default='Please Update Bay #')
+    db_type     = models.CharField(max_length=100,default='Type of Building')
+    db_reserved = models.CharField(max_length=3,default='Yes/No')
+    db_period   = models.CharField(max_length=100,blank=True)
+    db_price    = models.CharField(max_length=100, blank=True)
     list_status = (
         ('Approved', 'Approved'),
         ('Occupied', 'Occupied'),
         ('Pending' , 'Pending'),
     )    
-    db_status = models.CharField(max_length=100, choices=list_status, default='Pending')
-    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
-    serial_no = models.CharField(max_length=15, blank=True)
+    db_status   = models.CharField(max_length=100, choices=list_status, default='Pending')
+    timestamp   = models.DateTimeField(auto_now=False, auto_now_add=True)
+    serial_no   = models.CharField(max_length=15, blank=True)
 
     def __str__(self):
         return self.db_property
