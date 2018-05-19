d20d87cd919b57d0a3830159f82e8e4664e8d4c2
Wong Jao Ern <jao.ern.wong@intel.com>
2018-05-19 22:13:06 +0800

    changes made to db
    changes made to db

--- django-src/rent/models.py
+++ django-src/rent/models.py
@@ -11,22 +11,25 @@ from django.utils import timezone
 
 # Create your models here.
 class ParkingForRent (models.Model):
-    user        = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    db_property = models.CharField(max_length=100)
-    db_area     = models.CharField(max_length=200,blank=True)
-    db_bay      = models.CharField(max_length=100,default='Please Update Bay #')
-    db_type     = models.CharField(max_length=100,default='Type of Building')
-    db_reserved = models.CharField(max_length=3,default='Yes/No')
-    db_period   = models.CharField(max_length=100,blank=True)
-    db_price    = models.CharField(max_length=100, blank=True)
-    list_status = (
+    user         = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    db_property  = models.CharField(max_length=10000)
+    db_address   = models.CharField(max_length=100000, default="Address")
+    db_longitude = models.CharField(max_length=100000, default="Longitude")
+    db_latitude  = models.CharField(max_length=100000, default="Latitude")
+    db_area      = models.CharField(max_length=200,blank=True)
+    db_bay       = models.CharField(max_length=100,default='Please Update Bay #')
+    db_type      = models.CharField(max_length=100,default='Type of Building')
+    db_reserved  = models.CharField(max_length=3,default='Yes/No')
+    db_period    = models.CharField(max_length=100,blank=True)
+    db_price     = models.CharField(max_length=100, blank=True)
+    list_status  = (
         ('Approved', 'Approved'),
         ('Occupied', 'Occupied'),
         ('Pending' , 'Pending'),
     )    
-    db_status   = models.CharField(max_length=100, choices=list_status, default='Pending')
-    timestamp   = models.DateTimeField(auto_now=False, auto_now_add=True)
-    serial_no   = models.CharField(max_length=15, blank=True)
+    db_status    = models.CharField(max_length=100, choices=list_status, default='Pending')
+    timestamp    = models.DateTimeField(auto_now=False, auto_now_add=True)
+    serial_no    = models.CharField(max_length=15, blank=True)
 
     def __str__(self):
         return self.db_property
